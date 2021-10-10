d6a1811d327e38b29d81c89fb3d60dc0d0cd3d6c
cecyRdz <92197205+cecyRdz@users.noreply.github.com>
2021-10-10 10:14:34 -0500

    Segundo avance

--- Controlador/app.py
+++ Controlador/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__, template_folder='../vista')
 
 @app.route('/')
@@ -7,7 +7,29 @@ def inicio():
 
 @app.route('/consultarProductos')
 def consultarProductos():
-    return render_template('Productos/consultar.html')
+    return render_template('Productos/consultarProductos.html')
+
+@app.route('/registrarProducto')
+def registrarProducto():
+    return render_template('Productos/registrarProducto.html')
+
+@app.route('/registrarNuevoProducto',methods=['post'])
+def registrarNuevoProducto():
+    nombre = request.form['nombre']
+    return 'Se guardaron los cambios del producto: '+nombre
+
+@app.route('/ModificarProducto')
+def ModificarProducto():
+    return render_template('Productos/editarProducto.html')
+
+@app.route('/recopilarDatosEditados',methods=['post'])
+def recopilarDatosEditados():
+    idProducto = request.form['idProducto']
+    return 'Se guardaron los cambios del producto: '+idProducto
+
+@app.route('/EliminarProducto')
+def EliminarProducto():
+    return render_template('Productos//EliminarProducto.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
